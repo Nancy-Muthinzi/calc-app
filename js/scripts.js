715bf6fb2f9b6b4aa5407c93734db4d304c41f58
Nancy-Muthinzi <nanciekathini@gmail.com>
2018-07-12 12:14:30 +0300

    create addition function

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,23 @@
+
+
+
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
       var noString = $("input#input1").val();
-      var noArray = parseInt[noString.split(",")];
-      alert(noArray[1]);
-  });
+      var addition = parseInt(noString.split("+")};
+      var sum = 0;
+      var add = function(addition){
+        for (var number=0; number <=addition.length-1; number++){
+          sum += parseInt(addition[number]);
+          {
+        console.log(sum);
+        }
+      }
+  //     var sum = 0;
+  //     var add = noArray.map(num =>{
+  //       return sum +- num
+  //     });
+  //     alert(sum);
+  // });
 });
